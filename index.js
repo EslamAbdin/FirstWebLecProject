459c3870f019af3d17eadcbc9e243d5e477fdb28
EslamAbdin <eslamabdin2019@gmail.com>
2022-11-21 22:34:17 +0200

    make the hover start from where the mouse is

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const btnE1 = document.querySelector(".btn");
 /*add case when hover by the mouse*/
 btnE1.addEventListener("mouseover", ()=> {
+    const x = event.pageX - btnE1.offsetLeft;
+    const y = event.pageY - btnE1.offsetTop;
 
-
-
+    btnE1.style.setProperty("--xPos", x + "px");
+    btnE1.style.setProperty("--yPos", y + "px");
 })
